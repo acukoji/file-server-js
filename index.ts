09ce15c60c3ec82aafdf2d462491c59a54e04a28
Acukoji <acukoji@gmail.com>
2020-03-08 17:20:34 -0600

    added starting log

--- index.ts
+++ index.ts
@@ -3,7 +3,9 @@ import express, { response } from 'express';
 var app: express.Express = express();
 
 const PORT = process.env.PORT || '3000';
+
+console.log('server starting....');
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}!`);;
-    console.log('server started.')
+    console.log(`Example app listening on port ${PORT}!`);
+    console.log('server started.');
 });
