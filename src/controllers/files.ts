7692bf49f684c3b7aa3efb2fab52aa7b36def907
Acukoji <acukoji@gmail.com>
2020-03-31 20:13:06 -0600

    added integration test for uploads.

--- src/controllers/files.ts
+++ src/controllers/files.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import fileUpload from 'express-fileupload';
 
 module.exports = (app: express.Express) => {
+    // serves files located in "public" directory
     app.use(express.static('public'));
 
+    // endpoint to accept file uploads
     app.post('/files.json',
         async (req: express.Request, res: express.Response) => {
+            //making sure that there is a file
             if (!req.files) {
                 res
                     .status(400)
@@ -21,7 +24,8 @@ module.exports = (app: express.Express) => {
             const file: fileUpload.UploadedFile = (req.files.file.constructor == Array) ?
                 (<fileUpload.UploadedFile[]>req.files.file)[0] :
                 (<fileUpload.UploadedFile>req.files.file);
-
+            
+            //moving file from temp directory to where we want it to be stored
             file.mv('./public/files/' + file.name);
 
             res
@@ -32,4 +36,8 @@ module.exports = (app: express.Express) => {
             });
         }
     );
+
+    // create delete endpoint that prints the file name to be deleted
+    // $ curl -X DELETE localhost:3000/files/newPatient.pdf
+    // deleted newPatient.pdf
 };
--- tests/files.integration.ts
+++ tests/files.integration.ts
@@ -2,7 +2,9 @@
 // configure and create an app server once by importing index
 // this instance will be reused for every test.
 import request from 'supertest';
+import fs from 'fs';
 import http from 'http';
+import { v4 } from 'uuid';
 import { app } from '../src/app';
 
 // run integ tests
@@ -32,16 +34,37 @@ describe('files', () => {
     });
 
     it('photo.jpg', async () => {
-       const resp = await request(server)
-           .get('/files/photo.jpg');
+        const resp = await request(server)
+            .get('/files/photo.jpg');
 
-       // assert 200 http status code to confirm server is running
-       // for endpoints other than the root, you also get
-       // information on what type of in
-       expect(resp.status).toBe(200);
+        // assert 200 http status code to confirm server is running
+        // for endpoints other than the root, you also get
+        // information on what type of in
+        expect(resp.status).toBe(200);
 
-       const photoByteLength = 2111089;
-       const photoBuffer: Buffer = resp.body;
-       expect(photoBuffer.length).toBe(photoByteLength);
+        const photoByteLength = 2111089;
+        const photoBuffer: Buffer = resp.body;
+        expect(photoBuffer.length).toBe(photoByteLength);
+    });
+
+    it('upload file', async () => {
+        const uuid = v4();
+        const filePath = '/tmp/test-upload.txt';
+        // create tmpfile with unique number 
+        await fs.promises.writeFile(filePath, uuid);
+
+        // upload tmp file
+        const uploadResp = await request(server)
+            .post('/files.json')
+            .attach('file', filePath);
+        expect(uploadResp.status).toBe(200);
+        expect(uploadResp.body.status).toBe(true);
+
+        // fetch uploaded file
+        const getResponse = await request(server)
+            .get('/files/test-upload.txt');
+        // assert contents of file
+        expect(getResponse.status).toBe(200);
+        expect(getResponse.text).toBe(uuid);
     });
 })
